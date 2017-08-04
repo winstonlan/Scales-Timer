ac4bd07470b3a263ec5a419ee766a1e82e18ece8
Winston Lan <winston.chunhua.lan@gmail.com>
2017-08-04 09:46:43 -0700

    Create UI Tabbed View.

---
+++ Scales Timer/TimerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scales Timer
+//
+//  Created by Winston Lan on 8/4/17.
+//  Copyright © 2017 winstonlan. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
