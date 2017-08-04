53bbf2f17963ea6196b85c115bf951d3f8668db6
Winston Lan <winston.chunhua.lan@gmail.com>
2017-08-04 10:46:24 -0700

    Implement Timer Model class.

---
+++ Scales Timer/Timer.swift
@@ -0,0 +1,30 @@
+//
+//  Timer.swift
+//  Scales Timer
+//
+//  Created by Winston Lan on 8/4/17.
+//  Copyright © 2017 winstonlan. All rights reserved.
+//
+
+import Foundation
+
+class Timer {
+    var currentSecond : Int = 0
+    var currentMinute : Int = 0
+    
+    func incrementSeconds() {
+        currentSecond += 1
+    }
+    
+    func incrementMinutes() {
+        currentMinute += 1
+    }
+    
+    func resetSeconds() {
+        currentSecond = 0
+    }
+    
+    func resetMinutes() {
+        currentMinute = 0
+    }
+}
