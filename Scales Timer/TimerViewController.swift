f31ff414c431889d28a9812b35033cdfa49b64d3
Winston Lan <winston.chunhua.lan@gmail.com>
2017-08-04 13:25:48 -0700

    Finish basic timer function.

--- Scales Timer/TimerViewController.swift
+++ Scales Timer/TimerViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class TimerViewController: UIViewController {
     @IBOutlet weak var minutesLabel: UILabel!
     @IBOutlet weak var secondsLabel: UILabel!
+    @IBOutlet weak var pauseResumeButton: UIButton!
     
     var timer = Timer()
     var stopWatch = Stopwatch()
     var isTimerRunning = false
+    var resumeTapped = false
 
 
     override func viewDidLoad() {
@@ -25,9 +27,31 @@ class TimerViewController: UIViewController {
     
     //MARK: IBActions for Timer Buttons
     @IBAction func startButtonPressed(_ sender: Any) {
-        runTimer()
+        if isTimerRunning == false {
+            runTimer()
+        }
+        
     }
     
+    @IBAction func pauseButtonPressed(_ sender: Any) {
+        if resumeTapped == false {
+            timer.invalidate()
+            resumeTapped = true
+            pauseResumeButton.setTitle("Resume", for: .normal)
+        }
+        else {
+            runTimer()
+            resumeTapped = false
+            pauseResumeButton.setTitle("Pause", for: .normal)
+        }
+    }
+    @IBAction func resetButtonPressed(_ sender: Any) {
+        timer.invalidate()
+        stopWatch.resetMinutes()
+        stopWatch.resetSeconds()
+        updateMinutesAndSeconds()
+        isTimerRunning = false
+    }
     
     //MARK: Update UI Methods
     func updateMinutesAndSeconds() {
@@ -48,6 +72,7 @@ class TimerViewController: UIViewController {
     //MARK: Helper Methods
     func runTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(TimerViewController.updateStopWatch)), userInfo: nil, repeats: true)
+        isTimerRunning = true
     }
     
     func updateStopWatch() {
